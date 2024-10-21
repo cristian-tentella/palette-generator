4de29d7a2cd3446d994a2f9fd013b233680aaf2b
Cristian Tentella <cristiantentella@tuta.com>
2024-10-21 22:51:13 +0200

    feat(components): add ColorTable component

---
+++ src/lib/components/ColorTable.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import ColorScale from "./ColorScale.svelte";
+  
+  let props: { children: Snippet, chroma: number, hues: number[], scaleSize: number } = $props()
+</script>
+
+<div id="container">
+  <div id="table">
+    {#each props.hues as hue}
+      <ColorScale chroma={props.chroma} hue={hue} size={props.scaleSize} />
+    {/each}
+  </div>
+
+  {@render props.children()}
+</div>
+
+<style>
+  #container {
+    display: flex;
+    flex-direction: row;  
+    gap: 6rem;
+  }
+
+  #table {
+    display: flex;
+    flex-direction: row;
+    gap: 3rem;
+  }
+</style>
