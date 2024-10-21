651f44a0084663b869c863dec93038f6d43fa026
Cristian Tentella <cristiantentella@tuta.com>
2024-10-21 22:50:18 +0200

    feat(components): add components based on Slider

---
+++ src/lib/components/ChromaSlider.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { maxLCHChroma, minLCHChroma } from "$lib/ts/color";
+  import Slider from "./Slider.svelte";
+
+  let { chroma = $bindable() }: { chroma: number } = $props();
+</script>
+
+<Slider
+  name="chroma"
+  bind:value={chroma}
+  min={minLCHChroma}
+  max={maxLCHChroma}
+/>
---
+++ src/lib/components/SizeSlider.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Slider from "./Slider.svelte";
+
+  let { size = $bindable() }: { size: number } = $props();
+</script>
+
+<Slider name="size" bind:value={size} min={2} max={16} />
