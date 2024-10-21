c292f2c510976e9aa13d7a6589a5a9a07bec53e1
Cristian Tentella <cristiantentella@tuta.com>
2024-10-21 22:50:40 +0200

    feat(components): add ColorSwatch component

---
+++ src/lib/components/ColorSwatch.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { type ColorObject, display } from "colorjs.io/fn";
+
+  let props: { color: ColorObject } = $props();
+</script>
+
+<div id="swatch" style:background-color={display(props.color)}></div>
+
+<style>
+  #swatch {
+    width: 5rem;
+    height: 5rem;
+  }
+</style>
