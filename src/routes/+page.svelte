99b7625117f4285d15077b9694d5f9b95a112637
Cristian Tentella <cristiantentella@tuta.com>
2024-10-23 15:34:52 +0200

    feat: add preview of current swatch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
   import Palette from "$lib/components/domain/display/Palette.svelte";
+  import Swatch from "$lib/components/domain/display/Swatch.svelte";
   import AddSwatch from "$lib/components/domain/interaction/AddSwatch.svelte";
   import PaletteOptions from "$lib/components/domain/interaction/PaletteOptions.svelte";
   import SwatchOptions from "$lib/components/domain/interaction/SwatchOptions.svelte";
+  import { makeSwatch } from "$lib/domain/palette/utils";
+  import { options } from "$lib/state/options.svelte";
+  import { swatchesOptions } from "$lib/state/swatchesOptions.svelte";
+  import { swatchOptions } from "$lib/state/swatchOptions.svelte";
+  
+  let swatch = $derived(
+    makeSwatch(
+      options.baseLightness,
+      options.targetLightness,
+      swatchOptions.chroma,
+      swatchOptions.hue,
+      options.rows
+    )
+  )
 </script>
 
 <main>
-  <div>
+  <div id="options">
     <PaletteOptions />
     <SwatchOptions />
     <AddSwatch />
+    <button onclick={() => swatchesOptions.sort((a, b) => a.hue - b.hue)}>Sort</button>
   </div>
-  <Palette />
+  <div id="palette">
+    <Palette />
+    <Swatch {swatch} />
+  </div>
 </main>
 
 <style>
@@ -23,10 +42,15 @@
     gap: 4rem;
   }
 
-  div {
+  #options {
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 2rem;
+  }
+  
+  #palette {
+    display: flex;
+    gap: 8rem;
   }
 </style>
